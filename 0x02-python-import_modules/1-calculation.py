8c14bc56334daacadbccd91e9f0559be2f8c42cd
itsmuriuki <grdmuriuki@gmail.com>
2021-05-29 07:05:00 +0000

    removing print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the sum, difference, multiple and quotient of 10 and 5."""
+    """sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
