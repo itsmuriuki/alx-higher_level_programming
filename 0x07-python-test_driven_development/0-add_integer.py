edf4b3fe7c0ebf94dc8f32e6be3d5bcea74af6ff
itsmuriuki <grdmuriuki@gmail.com>
2021-06-17 15:02:27 +0000

    fix bugsin TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Documentation for a simple  add integer"""
 
 
@@ -12,9 +13,9 @@ def add_integer(a, b=98):
         the sum of a and b
     """
 
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
